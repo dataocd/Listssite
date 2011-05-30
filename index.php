e071e4a483b8c5352dc3f588ce4ebdcb2c849cc6
infolock <jhibbard@gmail.com>
2011-05-30 15:01:24 -0400

    Updated index to reflect changes to frontController

--- index.php
+++ index.php
@@ -9,17 +9,10 @@
 //This is where the magic happens...
 
 include('/opt/local/apache2/www/code/ListFramework/AutoLoader.php');
-$loader = Lists\AutoLoader::getInstance();
-$loader->registerNamespace('Zend','/var/apps/list/lib/Zend/');
-$loader->register();
 
-$front = Lists\FrontController::getInstance();
-//$front->run('/var/apps/listerthingamabob/packages/');
+$autoloader = Lists\AutoLoader::getInstance();
+$autoloader->register();
 
-//$response = $front->execute(); //Request object is implied. The controller will load up a default http request object
-
-//$response->sendResponse(); //Need to account for headers and html/xml/whatever
-
-
-//And that should be it.
-echo "Bullshit goes here";
+Lists\FrontController::run();
+// something getting echoed out here...
+?>
