dea0d6c55488fad31b8d76c4042dec4314c4f4da
JamesWPhillips <james@jameswphillips.com>
2011-05-30 14:55:58 -0400

    Synced with newest framework code

--- index.php
+++ index.php
@@ -8,16 +8,18 @@
 //                  /config  << need to have some config stuff some day
 //This is where the magic happens...
 
-include('/var/apps/list/lib/List/AutoLoader.php');
-$loader = List\AutoLoader::getInstance();
-$loader->registerNamespace('Zend','/var/apps/list/lib/Zend/')->register();
+include('/opt/local/apache2/www/code/ListFramework/AutoLoader.php');
+$loader = Lists\AutoLoader::getInstance();
+$loader->registerNamespace('Zend','/var/apps/list/lib/Zend/');
+$loader->register();
 
-$front = new List/FrontController();
-$front->run('/var/apps/listerthingamabob/packages/');
+$front = Lists\FrontController::getInstance();
+//$front->run('/var/apps/listerthingamabob/packages/');
 
-$response = $front->execute(); //Request object is implied. The controller will load up a default http request object
+//$response = $front->execute(); //Request object is implied. The controller will load up a default http request object
 
-$response->sendResponse(); //Need to account for headers and html/xml/whatever
+//$response->sendResponse(); //Need to account for headers and html/xml/whatever
 
 
 //And that should be it.
+echo "Bullshit goes here";
